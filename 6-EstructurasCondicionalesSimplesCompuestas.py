333d1bcd4d594ac01c56ef1e1dbb524dc4090574
FabiamDev <fabiam23@gmail.com>
2023-12-17 13:10:46 -0400

    Update 6-EstructurasCondicionalesSimplesCompuestas.py

--- 6-EstructurasCondicionalesSimplesCompuestas.py
+++ 6-EstructurasCondicionalesSimplesCompuestas.py
@@ -1,2 +1,12 @@
-print("Hola")
-##hhhh
+#Problema 1
+#Condicional Simple
+#Ingresar el sueldo de una persona, si supera los $3,000 mostrar un mensaje en pantalla
+#indicando que debe abonar impuestos
+
+sueldo = int(input("Ingrese el sueldo: "))
+if sueldo > 3000:
+    print("Esta persona debe abonar impuestos")
+
+#Condicional Compuesta
+#Realizar un programa que solicite ingresar dos numeros distintos y muestre el mayor de ellos
+
