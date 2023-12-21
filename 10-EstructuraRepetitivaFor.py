6cd818498b288d6f31c6091e1fb78b1f784c7bb9
fabiam23 <fabiam23@gmail.com>
2023-12-21 16:51:05 -0400

    Fabiam Work

--- 10-EstructuraRepetitivaFor.py
+++ 10-EstructuraRepetitivaFor.py
@@ -2,3 +2,15 @@
 
 for x in range(101):
     print(x)
+
+ # Realiza un programa que imprima en pantalla los numeros del 20 al 30   
+    
+for x in range(20,31):
+    print(x)
+
+# Imprimir todos los numeros impares que hay entre 1 y 100
+    
+for x in range(1,100,2):
+    print(x)
+
+#
