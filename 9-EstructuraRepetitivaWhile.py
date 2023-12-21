28e6519d52d62e9f338f0282c613e5c5f10b4a5a
fabiam23 <fabiam23@gmail.com>
2023-12-21 10:57:22 -0400

    Update 9-EstructuraRepetitivaWhile.py

--- 9-EstructuraRepetitivaWhile.py
+++ 9-EstructuraRepetitivaWhile.py
@@ -38,7 +38,71 @@ piezasAptas = 0
 while p < piezas:
     tamanoPieza = float(input("Ingrese el tamano de la pieza: "))
 
-    if tamanoPieza >= 1.2 and tamanoPieza <= 1.3:
+    if tamanoPieza >= 1.20 and tamanoPieza <= 1.30:
         piezasAptas = piezasAptas + 1
     p = p + 1
-print("Cantidad de piezas buenas",piezas, "de",piezas,"piezas")
+print("Cantidad de piezas buenas",piezasAptas, "de",piezas,"piezas")
+
+#PROBLEMAS PROPUESTOS
+
+# 9-1 Escribir un programa que cargue 10 notas de alumnos e informe cuantos tienen
+# notas mayores a igual a 7 y cuantos menores
+
+acumulador = 1
+mayor = 0
+menor = 0
+
+while acumulador <= 10:
+    notas = int(input("Ingrese la nota: "))
+
+    if notas >= 7:
+        mayor = mayor + 1
+    else:
+        menor = menor + 1
+    
+    acumulador = acumulador +1
+
+print(" Mayor de 7:",mayor)
+print("Menor de 7:", menor)
+
+# 9-2 Se ingresan un conjunto de n alturas. Mostrar la altura promedio
+
+acumulador1 = 1
+totalAlturas = 0
+cantidadAltura = int(input("Cuantas estaturas desea registrar: "))
+
+while acumulador1 <= cantidadAltura:
+    altura = float(input("Ingrese la altura: "))
+    totalAlturas = totalAlturas + altura
+    acumulador1 = acumulador1 + 1
+
+promedio = totalAlturas / cantidadAltura
+print("Total altura:", totalAlturas)
+print("Altura promedio:", promedio)
+
+# 9-3 En una empresa trabajan n empleados con sueldos entre 100 y 500, realizar 
+# un programa que lea los sueldos de cada empleado y mostrar cuantos cobran entre
+# 100 y 300 y cuantos mas de 300. Y mostrar cuanto gasta la empresa en total
+
+acumulador2 = 1
+totalsueldo = 0
+sueldo100_300 = 0
+sueldoMayor = 0
+
+cantidadEmpleados = int(input("Cuantos empleados hay en la empresa: "))
+while acumulador2 <= cantidadEmpleados:
+    sueldo = int(input("Ingrese el sueldo: "))
+    totalsueldo = totalsueldo + sueldo
+
+    if sueldo >= 100 and sueldo <= 300:
+        sueldo100_300 = sueldo100_300 + 1
+    else: 
+        if sueldo > 300:
+            sueldoMayor = sueldoMayor + 1
+    acumulador2 = acumulador2 + 1
+
+print("Total en sueldo:", totalsueldo)
+print("Sueldo entre 100 y 300:", sueldo100_300)
+print("Sueldo mayor 300", sueldoMayor)
+
+# 9-4
