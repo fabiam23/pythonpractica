a07b495d6cdb10b29d2a0a3af27b9d3b73ad55c5
FabiamLinux <fabiam23@gmail.com>
2024-01-10 23:20:50 -0400

    Update 15-ListasCargarPorTecladoElementos.py

--- 15-ListasCargarPorTecladoElementos.py
+++ 15-ListasCargarPorTecladoElementos.py
@@ -1,1 +1,24 @@
-# Una lista en python es mutable
+# Una lista en python es mutable. Puede ir cambiando durante la ejecusion
+"""
+lista = [10, 20, 40]
+print(len(lista))
+lista.append(100)
+print(len(lista))
+print(lista[0])
+print(lista[3])
+
+# Problema 1
+# Definir una lista vacia y luego solicitar la carga de 5 enteros por teclado 
+# y agregarlos a la lista. Imprimir la lista generada
+
+lista1 = []
+
+contador = 0
+while contador < 5:
+    elemento = int(input('Ingrese un valor: '))
+    lista1.append(elemento)
+    contador = contador + 1
+    
+print(lista1)
+
+"""
