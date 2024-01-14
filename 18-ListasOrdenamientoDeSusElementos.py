4d5e5f96a91a66578ede14de46c67d6c508e80fa
FabiamLinux <fabiam23@gmail.com>
2024-01-14 00:04:53 -0400

    Update 18-ListasOrdenamientoDeSusElementos.py

--- 18-ListasOrdenamientoDeSusElementos.py
+++ 18-ListasOrdenamientoDeSusElementos.py
@@ -1,4 +1,48 @@
 # Otro algoritmo muy comun que se debe conocer es el ordenamiento de una lista de datos
 
 # Problema 1
-#
+# Se debe cargar unalista donde se almacenen 5 sueldos. 
+# Desplazar el valor mayor de la lista a la ultima posicion.
+
+"""
+
+lista = []
+
+for x in range(5):
+    sueldo = int(input('Ingrese el sueldo: '))
+    
+    lista.append(sueldo)
+    
+print(lista)
+
+valor = 0
+
+for p in range(4):
+    for x in range(4):
+        if lista[x] > lista[x + 1]:
+            valor = lista[x]
+            lista[x] = lista[x+ 1]
+            lista[x + 1]=valor
+        
+print(lista)
+
+# PROBLEMAS PROPUESTOS
+
+"""
+# Problema 1
+# 
+lista = []
+
+for x in range(5):
+    v = int(input('valor: '))
+    lista.append(v)
+print(lista)
+
+for x in range(4):
+    for p in range(4 - x):
+        if lista[p] > lista[p + 1]:
+            aux = lista[p]
+            lista[p] = lista[p + 1]
+            lista[p + 1] = aux
+            
+print(lista)
