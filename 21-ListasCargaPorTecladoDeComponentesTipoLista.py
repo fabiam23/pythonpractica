ab95d19506cfb4e069f12546773058ed72f2003d
fabiam23 <fabiam23@gmail.com>
2024-01-19 16:17:20 -0400

    Update 21-ListasCargaPorTecladoDeComponentesTipoLista.py

--- 21-ListasCargaPorTecladoDeComponentesTipoLista.py
+++ 21-ListasCargaPorTecladoDeComponentesTipoLista.py
@@ -22,7 +22,7 @@ for n in range(3):
         
 for x in range(len(alumnos)):
     print(alumnos[x], notas[x][0], notas[x][1])
-"""
+
 # Problema 2
 # Se tiene que cargar la siguiente informacion:
 # Nombre de 3 empleados
@@ -41,3 +41,47 @@ for x in range(len(alumnos)):
 
 empleados = []
 sueldos = []
+
+for x in range(3):
+    nombre = input("Ingrese su nombre: ")
+    sueldo1 = int(input('Sueldo1: '))
+    sueldo2 = int(input('Sueldo2: '))
+    sueldo3 = int(input('Sueldo3: '))
+
+    empleados.append(nombre)
+    sueldos.append([sueldo1, sueldo2, sueldo3])
+
+for x in range(3):
+    print(empleados[x], sueldos[x])
+
+totalSueldos = []
+for x in range(len(sueldos)):
+     suma = sueldos[x][0] + sueldos[x][1] + sueldos[x][2]
+     totalSueldos.append(suma)
+       
+print(totalSueldos)
+
+
+nombres = 0
+mayor = totalSueldos[0]
+for x in range(1,3):
+     if totalSueldos[x] > mayor:
+          mayor = totalSueldos[x]
+          nombres = x
+
+print(empleados[nombres])
+         
+    
+"""
+# Problema 3
+# Solicitar por teclado dos enteros. El primer valor indica la cantidad de elementos que 
+# crearemos en la lista. El segundo valor indica la cantidad de elementos que tendra cada lista interna
+# Mostrar la lista y la suma de todos sus elementos.
+# ej 2 y 4
+# lista = [[1,2,3,4], [1,2,3,4]]
+
+tamanoLista = int(input('Cantidad de elementos de la lista: '))
+tamanoSubLista = int(input('Cantidad de elementos de la sublista: '))
+
+lista = []
+
