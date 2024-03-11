cc66fbe7347748216e128595a880bc13dbc246ac
FabiamDev <fabiam23@gmail.com>
2024-03-10 22:57:56 -0400

    Create 23-conceptosDeFunciones.py

---
+++ 23-conceptosDeFunciones.py
@@ -0,0 +1,21 @@
+#Confeccionar un programar que muestre una presentacion en pantalla
+#Solicite la carga de 2 valores y nos muestre la suma
+#Mostrar un mensaje de despedida
+#Implementar estas actividades en 3 funciones
+
+
+def suma():
+    v1 = int(input("Ingrese el primer valor: "))
+    v2 = int(input("Ingrese el segundo valor: "))
+    print("La suma es:", v1 + v2)
+
+def inicio():
+    print("**********\nHola\n**********")
+
+def saliendo():
+    print("**********\nByeeee\n**********")
+
+inicio()
+suma()
+saliendo()
+    
